cffe36d561317a99f7a13260231a861dfa20d113
Reginald201 <reginaldekeokparanta@gmail.com>
2026-06-03 12:41:45 +0100

    Added Output.java and other java files

---
+++ AskTeacher.java
@@ -0,0 +1,5 @@
+public class AskTeacher {
+	public static void main(String[] args) {
+	    System.out.println("Teacher can you pleae explain how loops?");
+	}
+}
---
+++ Output.java
@@ -0,0 +1,12 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone, this is my first Java code");
+        System.out.println("Learning Java is fun");
+        System.out.print("Here I am using the print method. ");
+        System.out.print("This text will be appended");
+        System.out.printf("%nMercy Ben is %d years old.%n", 25);
+        System.out.printf("My account balance is $%,.2f%n", 76549999.922276);
+        System.out.println("Benjamin is a boy");
+        System.out.printf("Do you love java? %b%n", true);
+    }
+}

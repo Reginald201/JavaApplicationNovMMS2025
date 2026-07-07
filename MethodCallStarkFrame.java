a15820e10066f9ee863ea28bfbefe393dd44e40e
Reginald201 <reginaldekeokparanta@gmail.com>
2026-07-07 13:31:08 +0100

    (MethodCallStarkFrame.java)

---
+++ MethodCallStarkFrame.java
@@ -0,0 +1,28 @@
+public class MethodCallStarkFrame{
+	public static void main(String[] args){
+		int x =50;
+		add(20,100);
+		System.out.println(x);
+	}
+	
+	public static void add(int x, int y){
+		int sum = x + y;
+		System.out.println(sum);
+		check(15);
+		
+		
+	}
+    public static void check(int age){
+		
+		if(age <= 18){
+			System.out.println("Yes you are an adult");
+		
+		
+	    }
+		else{
+			System.out.println("No yo are not an adult");
+		}
+	}
+	
+}
+	

73acbc83cc070d363e265413e1488ea7aa6f8d4d
Reginald201 <reginaldekeokparanta@gmail.com>
2026-07-22 14:05:50 +0100

    DaysOfWeek

---
+++ DaysOfWeek.java
@@ -0,0 +1,11 @@
+public class DaysOfWeek {
+    public static void main(String[] args) {
+        System.out.println("SUNDAY");
+        System.out.println("MONDAY");
+        System.out.println("TUESDAY");
+        System.out.println("WEDNESDAY");
+        System.out.println("THURSDAY");
+        System.out.println("FRIDAY");
+        System.out.println("SATURDAY");
+    }
+}
